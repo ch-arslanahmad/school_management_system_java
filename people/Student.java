a34979addaf20eb7ae00806ed4e073d6ede630fc
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-05 15:42:12 +0500

    feat(Student): added setDetails() method

--- people/Student.java
+++ people/Student.java
@@ -1,18 +1,28 @@
 package school_management_system_java.people;
-import school_management_system_java.people.*;
-public class Student {
+import java.util.Scanner;
+public class Student extends Person {
 
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    @Override
     String getName() {
         return name;
     }
-
+    
+    @Override
     int getAge() {
         return age;
+    }
+
+
+    void setDetails(Scanner input) {
+        System.out.print("Enter Student name: ");
+        name = input.nextLine();
+        System.out.print("Enter Student Age: ");
+        age = input.nextInt();
     }
 
     
