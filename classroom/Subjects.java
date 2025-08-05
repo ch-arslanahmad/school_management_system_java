4323e7eca97a9c8f1fe277eab31c4ca5ff3c4838
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-05 15:44:34 +0500

    feat(Subject): added Subject class with class & subject related methods

---
+++ classroom/Subjects.java
@@ -0,0 +1,41 @@
+package school_management_system_java.classroom;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Subjects implements ClassRoom{
+
+    protected int classroom = 1;
+    ArrayList <String> subjects = new ArrayList<>();
+    int totalSubjects;
+
+    @Override
+    public void addClass(Scanner input) {
+        System.out.println("Class: " + classroom);
+        classroom++;
+        System.out.print("Total Number of subjects: ");
+        totalSubjects = input.nextInt();
+        addclassSubjects(input);
+    }
+
+    @Override
+    public int addSubject(Scanner input) {
+        System.out.println("Enter class#");
+        int a = input.nextInt();
+        if(a == classroom) {
+            System.out.println("Enter Subject: ");
+            subjects.add(input.nextLine());
+            totalSubjects++;
+        }
+        return totalSubjects;
+    }
+
+    @Override
+    public void addclassSubjects(Scanner input) {
+        for(int i = 0; i <totalSubjects; i++) {
+            System.out.println("Enter Subject name: ");
+            subjects.add(input.nextLine());
+        }
+    }
+
+}
