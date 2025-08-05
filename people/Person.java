149461423c77ef5f46f4305d464a8be879c33faa
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-05 09:53:04 +0500

    creating Person.java file

---
+++ people/Person.java
@@ -0,0 +1,10 @@
+package school_management_system_java.people;
+
+public abstract class Person {
+    protected String name;
+    protected int age;
+
+    abstract String getName();
+    abstract int getAge();
+    
+}
