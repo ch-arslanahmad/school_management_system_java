8ebaedad76cd6744d4b663acf35a27f28d53f553
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-05 09:53:28 +0500

    created Student.java file

---
+++ people/Student.java
@@ -0,0 +1,19 @@
+package school_management_system_java.people;
+import school_management_system_java.people.*;
+public class Student {
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    
+}
