786ca7583e4b62865561b07fa0ab5b77e4b0f12f
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-07 13:42:58 +0500

    fixed the sql query & added logging

--- database/testing.java
+++ database/testing.java
@@ -1,5 +1,33 @@
 import java.sql.*;
 import java.util.Scanner;
+import java.util.logging.*;
+
+    private static final Logger logger = Logger.getLogger(testing.class.getName());
+    private static Filehandler fh;
+    
+    static {
+    // 
+    try {
+        // so logging is not shown in console
+        LogManager.getLogManager().reset();
+        String a = "Authlog.txt";
+        File file = new File(a);
+        // if file does not exist, create it
+        if(!(file.exists())) {
+            file.createNewFile();
+        }
+        fh = new FileHandler(a, true);
+        logger.addHandler(fh);
+        fh.setFormatter(new SimpleFormatter());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    finally {
+        logger.info("Log File is created!");
+    }
+    }
+
+
 class testing {
     // method to add student in the database;
     boolean insertClass(Connection conn, Scanner input) {
@@ -7,11 +35,17 @@ class testing {
         classroom.addClass(input);
 
         try {
+            // writing query in a string variable
             String addClass = "INSERT INTO Class (ClassName) VALUES (?)";
+            
+            // creating a prepared statement: injects variables instead of a whole query
             PreparedStatement writesql = conn.prepareStatement(addClass);
 
-            //
-            writesql.setString(3, classroom.getClassroom());
+            // set values in the query
+            writesql.setString(1, classroom.getClassroom());
+
+            // execute the query
+            writesql.execute();
         } catch (Exception e) {
             System.out.println("Error adding data: ");
             e.printStackTrace();
