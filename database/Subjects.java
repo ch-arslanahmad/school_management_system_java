ea55e2d59d869e2191f13e8c03f8d295f48bd673
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-07 12:44:41 +0500

    added file from school management project

---
+++ database/Subjects.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Subjects{
+
+    protected String classroom;
+    ArrayList <String> subjects = new ArrayList<>();
+    int totalSubjects;
+
+    public void addClass(Scanner input) {
+        System.out.print("ClassName: ");
+        classroom = input.nextLine();
+        System.out.print("Total Number of subjects: ");
+        totalSubjects = input.nextInt();
+        input.nextLine();
+        addclassSubjects(input);
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public int addSubject(Scanner input) {
+        System.out.print("Enter class: ");
+        String a = input.nextLine();
+        if(a == classroom) {
+            System.out.print("Enter Subject: ");
+            subjects.add(input.nextLine());
+            totalSubjects++;
+        }
+        return totalSubjects;
+    }
+
+    public void addclassSubjects(Scanner input) {
+        for(int i = 0; i <totalSubjects; i++) {
+            System.out.print("Enter Subject name: ");
+            subjects.add(input.nextLine());
+        }
+    }
+
+}
