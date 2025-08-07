2fb445daa5858505cc56ac5b5dbe6043c075e0b1
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-07 18:54:23 +0500

    updated with SQL lite with core functionality to add data to database

--- database/testing.java
+++ database/testing.java
@@ -1,16 +1,21 @@
 import java.sql.*;
 import java.util.Scanner;
 import java.util.logging.*;
+import java.io.File;
+
+
+class testing {
 
     private static final Logger logger = Logger.getLogger(testing.class.getName());
-    private static Filehandler fh;
+    private static FileHandler fh;
     
+    // STATIC block for **LOGGING**
     static {
     // 
     try {
         // so logging is not shown in console
         LogManager.getLogManager().reset();
-        String a = "Authlog.txt";
+        String a = "testlog.txt";
         File file = new File(a);
         // if file does not exist, create it
         if(!(file.exists())) {
@@ -19,6 +24,7 @@ import java.util.logging.*;
         fh = new FileHandler(a, true);
         logger.addHandler(fh);
         fh.setFormatter(new SimpleFormatter());
+        logger.setLevel(Level.FINE);
     } catch (Exception e) {
         e.printStackTrace();
     }
@@ -27,12 +33,50 @@ import java.util.logging.*;
     }
     }
 
+    Connection conn;
+    // method to setupConnection
+    boolean setupConnection(Scanner input) {
+        boolean isConnected = false;
+        conn = null;
+        try {
+            // Load JDBC Driver
+            Class.forName("org.sqlite.JDBC");
+            // Connecting the database file
+            conn = DriverManager.getConnection("jdbc:sqlite:people.db");
+            // run only if connection is not null
+            if(conn != null) {
+                isConnected =  true;
+                logger.info("Connection Established");
+                // insert class in SQL lite
+                insertClass(conn, input);
+                
+            }
 
-class testing {
+        } catch (Exception e) {
+            System.out.print("Error establishing connection with database/sqlite: ");
+            e.printStackTrace();
+
+            logger.warning("Error establishing connection with database/sqlite");
+
+        }
+        finally {
+            try {
+               conn.close();             
+            } catch (Exception e) {
+                System.out.println("Connection not properly closed");
+                e.printStackTrace();
+            }
+        }
+        return isConnected;
+
+    }
+
+
     // method to add student in the database;
     boolean insertClass(Connection conn, Scanner input) {
         Subjects classroom = new Subjects();
         classroom.addClass(input);
+        boolean insertedClass = false;
 
         try {
             // writing query in a string variable
@@ -46,36 +90,42 @@ class testing {
 
             // execute the query
             writesql.execute();
+            insertedClass = true;
+
+            if(insertedClass) {
+                logger.info("Values Successfully Inserted.");
+                System.out.println("Successfully entered");
+                conn.close();
+            }
+            else {
+                System.out.println("ERROR Values NOT added.");
+                logger.warning("ERROR Values NOT added");
+            }
+
+
         } catch (Exception e) {
             System.out.println("Error adding data: ");
             e.printStackTrace();
+            logger.warning("Error adding data into databse");
         }
-        return true;
+        return insertedClass;
     }
+
+    // END logging
+    void endlog() {
+        fh.close();
+    }
+
+    // MAIN METHOD
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         testing test = new testing();
 
+        boolean isConnected = test.setupConnection(input);
 
-
-        Connection conn = null;
-
-        try {
-            // Load JDBC Driver
-            Class.forName("org.sqlite.JDBC");
-            // Connecting the database file
-            conn = DriverManager.getConnection("jdbc:sqlite:people.db");
-            // run only if connection is not null
-            if(!(conn == null)) {
-                // insert class in SQL lite
-                test.insertClass(conn, input);
-            }
-        } catch (Exception e) {
-            System.out.print("Errors: ");
-            e.printStackTrace();
-
+        if(isConnected) {
+            fh.close();
         }
-
     }
 
 }
