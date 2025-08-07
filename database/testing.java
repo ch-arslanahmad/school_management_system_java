3307f37bc5ce412dbdbf222e06c604c5a112e3d5
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-07 12:45:05 +0500

    feat(testing): modular code to connect & add data to SQL Lite

--- database/testing.java
+++ database/testing.java
@@ -1,22 +1,45 @@
 import java.sql.*;
 import java.util.Scanner;
 class testing {
+    // method to add student in the database;
+    boolean insertClass(Connection conn, Scanner input) {
+        Subjects classroom = new Subjects();
+        classroom.addClass(input);
 
+        try {
+            String addClass = "INSERT INTO Class (SubjectName) VALUES (?)";
+            PreparedStatement writesql = conn.prepareStatement(addClass);
 
+            //
+            writesql.setString(3, classroom.getClassroom());
+        } catch (Exception e) {
+            System.out.println("Error adding data: ");
+            e.printStackTrace();
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Student arslan = new Student();
-        arslan.setDetails(input);
+        testing test = new testing();
 
-        String name = arslan.getName();
-        int age = arslan.getAge();
+
 
         Connection conn = null;
 
         try {
-            
+            // Load JDBC Driver
+            Class.forName("org.sqlite.JDBC");
+            // Connecting the database file
+            conn = DriverManager.getConnection("jdbc:sqlite:people.db");
+            // run only if connection is not null
+            if(!(conn == null)) {
+                // insert class in SQL lite
+                test.insertClass(conn, input);
+            }
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.print("Errors: ");
+            e.printStackTrace();
+
         }
 
     }
