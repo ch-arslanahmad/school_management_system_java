2c0d8e679da552d19c1ad7f8bf62eeb8578be336
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-11 10:58:19 +0500

    feat(DAO): implement baseline ClassDAO

--- database/DAO/ClassDAO.java
+++ database/DAO/ClassDAO.java
@@ -1,11 +1,14 @@
+// BETA VERSION FINALIZED - ClassDAO
+package database.DAO;
+
+// package imports
+import database.*;
+import classroom.*;
 // imports
 import java.io.File;
 import java.sql.*;
 import java.util.Scanner;
 import java.util.logging.*;
-
-import school_management_system_java.classroom.*;
-import school_management_system_java.classroom.database.DBmaker;
 
 // public class
 public class ClassDAO {
@@ -13,129 +16,214 @@ public class ClassDAO {
     // variables for LOGGing
     private static final Logger logger = Logger.getLogger(ClassDAO.class.getName());
     private static FileHandler fh;
-    
+
     // STATIC block for **LOGGING**
     static {
-    // 
-    try {
-        // so logging is not shown in console
-        LogManager.getLogManager().reset();
-        String a = "testlog.txt";
-        File file = new File(a);
+        //
+        try {
+            /*
+             * // so logging is not shown in console
+             * LogManager.getLogManager().reset();
+             */
+            String a = "ClassDAO.txt";
+            File file = new File(a);
             // if file does not exist, create it
-            if(!(file.exists())) {
+            if (!(file.exists())) {
                 file.createNewFile();
             }
-        fh = new FileHandler(a, true);
-        logger.addHandler(fh);
-        fh.setFormatter(new SimpleFormatter());
-        logger.setLevel(Level.FINE);
+            fh = new FileHandler(a, true);
+            logger.addHandler(fh);
+            fh.setFormatter(new SimpleFormatter());
+            logger.setLevel(Level.FINE);
 
-        // checking if file exists
-        if(file.exists()) {
-            logger.info("Log File is created!");
+            // checking if file exists
+            if (file.exists()) {
+                logger.info("Log File is created!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
     }
 
+    String classNameDAO;
+    int classID;
+
+    ClassRoom room = new ClassRoom();
+
+    // method to get ID from Class (-1 Error Code)
+    public int getIDfromClass(String name) {
+        String classIDSQL = "SELECT ClassID FROM Class where ClassName = ?;";
+        // No reasonable ID will reach this ammount, hence the reason of this value
+        classID = -1;
+        try (PreparedStatement rm = Database.getConn().prepareStatement(classIDSQL)) {
+
+            rm.setString(1, name);
+
+            ResultSet rs = rm.executeQuery();
+
+            if (rs.next()) {
+                // fetches and stores the ClassID in a variable from the matched row
+                classID = rs.getInt("ClassID");
+                logger.info(name + " ID is: " + classID);
+            } else {
+                logger.config("Unable to get ClassID.");
+            }
+
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error while fetching ClassID.", e);
+        }
+
+        return classID;
+
+    }
 
     // see if class exists
-    public boolean Classexists(Scanner input) {
-
-        // get class name for comparison
-        System.out.println("Enter class name: ");
-        String classname = input.nextLine();
-
+    public boolean Classexists(String name) {
         // SQL query to check
-        String check = "SELECT COUNT(*) FROM Class WHERE ClassName = ?";
+        String check = "SELECT COUNT(*) AS count FROM Class WHERE ClassName = ?;";
 
-        // prepared statement
-        PreparedStatement rm = Database.getConn().prepareStatement(check);
+        // prepared statement in try block
+        try (PreparedStatement rm = Database.getConn().prepareStatement(check)) {
 
-        // adding value to query
-        rm.setString(1, classname);
+            // adding value to query
+            rm.setString(1, name);
 
-/*
-1. We use executeQuery() not executeUpdate() because we are not adding, removing anything we are simply executing the query.
-2.  executeQuery returns value in ResultSet Object hence that value is also stored ResultSet variable, 'rs'.
-*/
+            /*
+             * 1. We use executeUpdate() because we are not adding, removing anything we are
+             * simply executing the query to extract some value.
+             * 2. executeQuery returns value in ResultSet Object hence that value is also
+             * stored ResultSet variable, 'rs'.
+             */
 
-        ResultSet rs = rm.executeQuery();
+            ResultSet rs = rm.executeQuery();
 
-/*
-ResultSet (rs) returns a table even if its only 1 column as matching.
-It does not automatically point to the matched column. To do that you do rs.next().
-        - res.next() will return false if rs is empty due to no match.
-        - return true if it is not empty
-I used rs.getInt("ClassID"), it is self-explanatory. It gets the classid of the match. Useful if user wants to add data to the matched class.
+            /*
+             * ResultSet (rs) returns a table with one row.
+             * It does not automatically point to the matched column. To do that you do
+             * rs.next().
+             * - res.next() returns row.
+             * - true if more than 1
+             * - false if 0
+             * 
+             * 
+             * NOTE: THINK OF MAKING A METHOD TO SEE THE ID FROM NAME LIKE getIDClass() or
+             * smthing.
+             */
 
-NOTE: THINK OF MAKING A METHOD TO SEE THE ID FROM NAME LIKE getIDClass() or smthing.
- */
+            if (rs.next()) {
+                int count = rs.getInt("count");
+                if (count > 0) {
+                    System.out.println("Match found");
+                    return true;
+                } else {
+                    System.out.println("No match found");
+                    return false;
+                }
+            }
 
-        if(rs.next()) {
-            System.out.println("Match found");
-            int classid = rs.getInt("ClassID");
-            return true;
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error finding Class Existance: ", e);
         }
-        else {
-            System.out.println("No match found");
-        }
-
         return false;
+    }
+
+    // method to get Validated ID from Class (-1 Error Code)
+    public int getValidClassID(String name) {
+        // getID only if class exists
+        if (!Classexists(name)) {
+            logger.info("Class Does Not exist");
+            return -1;
+        } else {
+            // return the ID from class
+            return getIDfromClass(name);
+        }
 
     }
 
     // to insert 1 class
-    public boolean insertClass(Scanner input) {
-        try {
-            ClassRoom room = new ClassRoom();
-            // input class name
-            room.addClass(input);
+    public boolean insertClass(String name) {
+        String classSQL = "INSERT INTO Class (ClassName) VALUES (?)";
+        try (PreparedStatement rm = Database.getConn().prepareStatement(classSQL);) {
             // SQL query
-            String classSQL = "INSERT INTO Class (ClassName) VALUES (?)";
-            PreparedStatement rm = Database.getConn().prepareStatement(classSQL);
 
             // set values in the query
-            rm.setString(1, room.getClassroom());
+            rm.setString(1, name);
 
             // execute query
-            rm.executeUpdate();
+            int rs = rm.executeUpdate();
 
-            // confirmation
-            System.out.println("Class Added");
-            return true;       
+            if (rs > 0) {
+                // confirmation
+                logger.info("Class Added");
+                return true;
+            } else {
+                logger.config("Class Not Added.");
+                return false;
+            }
+
         } catch (Exception e) {
+            // writing text with errors
+            logger.log(Level.WARNING, "Error while Inserting Class: ", e);
         }
         return false;
     }
 
-
     // to insert many classes
     public boolean insertClasses(Scanner input) {
-        // adding Class
-        while (true) {      
+        // adding Classes
+        while (true) {
             try {
-            boolean classAdded = insertClass(input);
-                if(classAdded) {
-                    logger.info("Class Successfully entered");
-                }
-                else {
+                boolean classAdded = insertClass(room.setClass(input));
+                if (classAdded) {
+                    logger.finest("Class Successfully entered");
+                } else {
                     System.out.println("Class must be entered to continue.");
                     continue;
                 }
+
+                // to stop the loop (user enters 0)
+                // used string as input as using any other type may cause newline buffer problem
                 System.out.println("Press 0 to stop.");
-                int c;
-                c = input.nextInt();
-                if(c == 0) {
-                    break;
+                String c;
+                c = input.nextLine();
+
+                if (c.equals("0")) {
+                    return true;
                 }
             } catch (Exception e) {
-                System.out.println("Error Adding Class Data");
+                logger.warning("Error Adding Class Data");
                 e.printStackTrace();
             }
         }
-    } 
-    
+    }
+
+    public boolean deleteClass(String name) {
+        if (!(Classexists(name))) {
+            System.out.println("No Match found");
+        } else {
+            String deleteClassSQL = "DELETE TABLE Class WHERE ClassName = ?;";
+            try (PreparedStatement rm = Database.getConn().prepareStatement(deleteClassSQL)) {
+
+                // set values in the query
+                rm.setString(1, name);
+
+                // execute query
+                int rs = rm.executeUpdate();
+
+                if (rs > 0) {
+                    // confirmation
+                    logger.info("Class Deleted");
+                    return true;
+                } else {
+                    logger.config("Class unable to delete.");
+                    return false;
+                }
+            } catch (Exception e) {
+                logger.log(Level.WARNING, "Error while deleting Class: ", e);
+            }
+        }
+
+        return false;
+
+    }
 }
