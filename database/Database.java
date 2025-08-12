a77f2654e019f80072d2cb4fbaf0cdf062a09837
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-12 20:32:09 +0500

    feat(DB) resolved errors and removed unused/redundant method

--- database/Database.java
+++ database/Database.java
@@ -1,4 +1,5 @@
 package database;
+
 import java.io.File;
 import java.sql.*;
 import java.util.logging.*;
@@ -9,52 +10,53 @@ public class Database {
     // variables for LOGGing
     private static final Logger logger = Logger.getLogger(Database.class.getName());
     private static FileHandler fh;
-    
+
     // STATIC block for **LOGGING**
     static {
-    // 
-    try {
-        // so logging is not shown in console
-        LogManager.getLogManager().reset();
-        String a = "testlog.txt";
-        File file = new File(a);
+        //
+        try {
+            // so logging is not shown in console
+            LogManager.getLogManager().reset();
+            String a = "testlog.txt";
+            File file = new File(a);
             // if file does not exist, create it
-            if(!(file.exists())) {
+            if (!(file.exists())) {
                 file.createNewFile();
             }
-        fh = new FileHandler(a, true);
-        logger.addHandler(fh);
-        fh.setFormatter(new SimpleFormatter());
-        logger.setLevel(Level.FINE);
+            fh = new FileHandler(a, true);
+            logger.addHandler(fh);
+            fh.setFormatter(new SimpleFormatter());
+            logger.setLevel(Level.FINE);
 
-        // checking if file exists
-        if(file.exists()) {
-            logger.info("Log File is created!");
+            // checking if file exists
+            if (file.exists()) {
+                logger.info("Log File is created!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    } catch (Exception e) {
-        e.printStackTrace();
     }
-    }
-
-
-
-
 
     // DATABASE CONNECTION
     private static Connection conn;
+
     // setupConnection of DB
-    static boolean setupConnection(Scanner input) {
+    static boolean setupConnection() {
         boolean isConnected = false;
         conn = null;
         try {
+            File dbFile = new File("database/people.db");
+            if (!dbFile.exists()) {
+                throw new SQLException("File Not found.");
+            }
             // Load JDBC Driver
             Class.forName("org.sqlite.JDBC");
             // Connecting the database file
-            conn = DriverManager.getConnection("jdbc:sqlite:people.db");
+            conn = DriverManager.getConnection("jdbc:sqlite:" + dbFile.getAbsolutePath());
             // run only if connection is not null
-            if(conn != null) {
-                isConnected =  true;
-                logger.info("Connection Established");                
+            if (conn != null) {
+                isConnected = true;
+                logger.info("Connection Established");
             }
             // if connection not made
             else {
@@ -68,28 +70,24 @@ public class Database {
             logger.warning("Error establishing connection with database/sqlite");
 
         }
-        finally {
-            try {
-               conn.close();             
-            } catch (Exception e) {
-                System.out.println("Connection not properly closed");
-                e.printStackTrace();
-            }
-        }
         return isConnected;
-
     }
+    // REMOVED conn.close() as its illogical to make a connection and close it right
+    // after.
 
     static void closeCon() {
         try {
-            conn.close();           
+            conn.close();
         } catch (Exception e) {
-            System.out.println("Error while closing connection: ");
-            e.printStackTrace();
+            logger.log(Level.WARNING, "Error while closing connection: ", e);
         }
     }
 
     public static Connection getConn() {
-        return conn;
+        if (setupConnection()) {
+            return conn;
+        } else {
+            return null;
+        }
     }
 }
