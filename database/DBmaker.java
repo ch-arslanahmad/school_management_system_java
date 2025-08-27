cdbc258757a7456cb6e4d139f36448bfb7cea7c4
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-27 12:50:43 +0500

    feat(DBmaker): added methods to add data in DB

--- database/DBmaker.java
+++ database/DBmaker.java
@@ -1,10 +1,10 @@
 package database;
 
 import database.DAO.*;
+import display.Input;
 
 import java.io.File;
-import java.sql.*;
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.logging.*;
 
 public class DBmaker {
@@ -17,7 +17,7 @@ public class DBmaker {
         //
         try {
             // so logging is not shown in console
-            LogManager.getLogManager().reset();
+            // LogManager.getLogManager().reset();
             String a = "log/DBmaker.txt";
             File file = new File(a);
             // if file does not exist, create it
@@ -38,16 +38,59 @@ public class DBmaker {
         }
     }
 
+    public static void closeLog() {
+        fh.flush();
+        fh.close();
+    }
+
     // METHOD to create whole database
-    void createDB(Scanner input) {
-        DBmaker db = new DBmaker();
-        ClassDAO classdb = new ClassDAO();
-        Database.setupConnection();
+    public void createDB(Input input) {
+        DBManager check = new DBManager();
 
-        System.out.println("To Use this APP you must create a database.");
-        System.out.println("First lets add a class or multiple classes.");
+        System.out.println(
+                "To Use this APP you must create a database. BUT BEFORE That, they may be dummy data beforehand in the DB.\n\n");
+        System.out.println("WARNING: ALL THE DATA WILL BE DELETED.");
+        check.removeAllData(input);
 
-        // add subjects to the classes
+        // all the objects of DB
+        SchoolDAO school = new SchoolDAO();
+        ClassDAO room = new ClassDAO();
+        StudentDAO student = new StudentDAO();
+        SubjectDAO subject = new SubjectDAO();
+        TeacherDAO teacher = new TeacherDAO();
+
+        // SCHOOL
+        System.out.println("Ok, First. \nTell me school name: ");
+        String schoolName = input.getStrInput();
+        System.out.println("Tell me School Principle name: ");
+        String principleName = input.getStrInput();
+
+        // if School-info is not inserted
+        if (!school.insertSchool(schoolName, principleName)) {
+            String error = "Database Creation Abort : School-Info";
+            logger.warning(error);
+            System.exit(0);
+        }
+
+        // arrayList of classes that were inserted
+
+        // CLASSES
+        System.out.println("Ok, NOW. \nLets add a class or multiple classes.\n");
+        ArrayList<String> classes = check.insertClassesDB(room, input);
+        System.out.println("All Classes Inserted.\n");
+
+        // STUDENTS
+        System.out.println("Now Students");
+        check.insertStudentsDB(classes, student, input);
+
+        // SUBJECTS
+        System.out.println("Now Subjects");
+        ArrayList<String> subjects = check.insertSubjectsDB(classes, subject, input);
+
+        // TEACHERS
+        System.out.println("Now Teachers");
+        check.insertTeachersDB(subjects, teacher, input);
 
     }
+
 }
